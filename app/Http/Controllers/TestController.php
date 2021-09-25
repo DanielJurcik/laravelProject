3f6de1b4f48fb6c9062274854ef383f751b9f0d1
Daniel <dano.jurcik@gmail.com>
2021-09-25 20:19:17 +0200

    1 - Test controller and Route

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Http\Controllers;
+
+class TestController extends Controller
+{
+    public function testFunction()
+    {
+        echo "fungujem";
+        echo "test";
+    }
+}
